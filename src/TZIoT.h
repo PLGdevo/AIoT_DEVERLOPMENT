0a6d81277ba9457539ffe10758a91591ae587d21
PLG <PLGgroup27@gmail.com>
2026-09-14 09:50:36 +0700

    chore: configure local test main.cpp in .gitignore and standardize platformio.ini

--- src/TZIoT.h
+++
@@ -1,6 +0,0 @@
-#ifndef INC_TZIoT_LIB
-#define INC_TZIoT_LIB
-
-#include <AIoT.h>
-
-#endif /*INC_TZIoT_LIB*/
--- src/TZIoT.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef INC_IoT_HPP
-#define INC_IoT_HPP
-
-#include <AIoT.hpp>
-
-#endif /*INC_IoT_HPP*/
